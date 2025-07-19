f67278bcdf402ff4900906c5b020283a4ebe04bb
snehalchinchghare <snehalchinchghare6@gmail.com>
2025-07-19 15:27:39 +0530

    added header links

--- src/app/features/dashboard/dashboard.routes.ts
+++ src/app/features/dashboard/dashboard.routes.ts
@@ -1,7 +1,36 @@
 import { Routes } from '@angular/router';
-import { CandidateLoginComponent } from './candidate-login/candidate-login.component';
 import { CandidateRegisterComponent } from './candidate-register/candidate-register.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { SendOfferMessageComponent } from './send-offer-message/send-offer-message.component';
+import { UpdatePackageComponent } from './update-package/update-package.component';
+import { DeleteCandidateComponent } from './delete-candidate/delete-candidate.component';
 
 export const dashboardRoutes: Routes = [
+  {
+    path: '',
+    component: DashboardComponent,
+    children: [
+      {
+        path: 'create-candidate',
+        component: CandidateRegisterComponent,
+      },
+      {
+        path: 'delete-candidate',
+        component: DeleteCandidateComponent,
+      },
+      {
+        path: 'update-package',
+        component: UpdatePackageComponent,
+      },
+      {
+        path: 'send-offer',
+        component: SendOfferMessageComponent,
+      },
+      //   {
+      //     path: '',
+      //     redirectTo: 'create-candidate',
+      //     pathMatch: 'full'
+      //   }
+    ],
+  },
 ];
