f50063d701615dbaaaea598378a52986e5711506
swapnil.h.wasnik <swapnil.wasnik@disa.com>
2025-07-25 20:37:53 +0530

    Fixed issue of localstorage

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { Router, RouterModule } from '@angular/router';
+import { SupabaseService } from '../../features/supabase/common.supabase.service';
 
 @Component({
   selector: 'app-header',
@@ -17,20 +18,21 @@ export class HeaderComponent implements OnInit {
   private readonly ADMIN_KEY = 'adminUser';
   private readonly TOKEN_KEY = 'adminToken';
 
-  constructor(public router: Router) {}
+  constructor(
+    public router: Router,
+    private supabaseService: SupabaseService) {}
 
-  ngOnInit(): void {
+  async ngOnInit() {
     const admin = localStorage.getItem(this.ADMIN_KEY);
     if (admin) {
       const parsed = JSON.parse(admin);
       this.adminName = parsed.fullname;
       this.gymName = parsed.gymName;
-      this.gymLogo = parsed.gymLogo;
-    }
 
-    // this.router.events.subscribe(() => {
-    //   this.isDashboardRoute = this.router.url.includes('/dashboard');
-    // });
+      this.supabaseService.getGymLogoByUserId(parsed.userId).then(logo => {
+        this.gymLogo = logo;
+      });
+    }
   }
 
   logout() {
