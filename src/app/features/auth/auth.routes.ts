d655ed1f216394676b2a9eca05409fda115fc89a
swapnil.h.wasnik <swapnil.wasnik@disa.com>
2025-07-29 02:33:56 +0530

    Added pricing page

--- src/app/features/auth/auth.routes.ts
+++ src/app/features/auth/auth.routes.ts
@@ -3,12 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { ReceiptComponent } from '../dashboard/receipt/receipt.component';
+import { PricingComponent } from '../pricing/pricing.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
   { path: 'receipt/:data', component: ReceiptComponent, },
+  { path: 'pricing', component: PricingComponent, },
 ];
 
 @NgModule({
