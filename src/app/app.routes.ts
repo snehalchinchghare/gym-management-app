2f3191c5f16a56070b9639905e4e6d3beeebcad8
swapnil.h.wasnik <swapnil.wasnik@disa.com>
2025-07-29 13:30:32 +0530

    WIP: Home Screen App

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,12 +1,8 @@
 import { Routes } from '@angular/router';
-import { LoginComponent } from './features/auth/login/login.component';
-import { RegisterComponent } from './features/auth/register/register.component';
-import { CandidateRegisterComponent } from './features/dashboard/candidate-register/candidate-register.component';
 import { AuthLayoutComponent } from './layouts/auth-layout/auth-layout.component';
 import { MainLayoutComponent } from './layouts/main-layout/main-layout.component';
 import { authGuard } from './core/auth.guard';
 import { PageNotFoundComponent } from './shared/page-not-found/page-not-found.component';
-import { ReceiptComponent } from './features/dashboard/receipt/receipt.component';
 
 export const routes: Routes = [
   {
--- src/main.ts
+++ src/main.ts
@@ -3,14 +3,22 @@ import { AppComponent } from './app/app.component';
 import { provideRouter } from '@angular/router';
 import { routes } from './app/app.routes';
 import { appConfig } from './app/app.config';
-import { ErrorHandler } from '@angular/core';
+import { ErrorHandler, isDevMode } from '@angular/core';
 import { GlobalErrorHandler } from './app/features/services/global-error-handler';
+import { provideServiceWorker } from '@angular/service-worker';
 
 bootstrapApplication(AppComponent, {
   ...appConfig,
   providers: [
     provideRouter(routes),
+    provideServiceWorker('ngsw-worker.js', {
+      enabled: true, // Enable in all environments
+    }),
     { provide: ErrorHandler, useClass: GlobalErrorHandler },
-    ...(appConfig.providers || [])
-  ]
+    ...(appConfig.providers || []),
+    provideServiceWorker('ngsw-worker.js', {
+        enabled: !isDevMode(),
+        registrationStrategy: 'registerWhenStable:30000'
+    })
+]
 }).catch((err) => console.error(err));
